34370620b880ebb15ddac66093f3b4850fb2b765
Paul-Alexandre Tessier <tessier.paulalexandre@orange.fr>
2025-04-11 17:02:50 +0200

    refactor: reorganize repository  (#1)
    * refactor: add ethereum core contracts
    
    * refactor: ethereum contracts
    
    * refactor: add rewardManager and Autoswapper ethereum
    
    * refactor: fix import path
    
    * refactor: add L2 contracts
    
    * refactor: autoswapper L2
    
    * refactor: rework autoswapper ethereum
    
    * refactor: add arbitrum contracts
    
    * refactor: cleanup
    
    * refactor: remove deploy folder
    
    * ci: update ci to not run tests anymore
    
    * docs: update README.md

--- test/FarmingRange/testData.ts
+++
@@ -1,5 +0,0 @@
-import { BigNumber } from "ethers";
-import { parseEther } from "ethers/lib/utils";
-
-export const INITIAL_BONUS_REWARD_PER_BLOCK = parseEther("100");
-export const BIGNUMBER_ZERO = BigNumber.from(0);
