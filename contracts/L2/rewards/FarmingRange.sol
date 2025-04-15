6fea92c371205a7624070679efaa3eafa143071f
Paulalex85 <tessier.paulalexandre@orange.fr>
2025-04-15 09:55:29 +0200

    fix: rework IERC20Permit import

--- contracts/L2/rewards/FarmingRange.sol
+++ contracts/L2/rewards/FarmingRange.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // interfaces
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import "./interfaces/IFarmingRange.sol";
 
 /**
--- contracts/ethereum/rewards/Staking.sol
+++ contracts/ethereum/rewards/Staking.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 // libraries
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
